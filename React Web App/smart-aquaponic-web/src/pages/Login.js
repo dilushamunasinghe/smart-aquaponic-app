c3bc993cbceb7c6702d2b8b7f73acd81ffb9bb4f
MunasingheTS <munasinghets93@gmail.com>
2021-12-20 20:33:12 +0530

    - mid work commit on login page

--- React Web App/smart-aquaponic-web/src/pages/Login.js
+++ React Web App/smart-aquaponic-web/src/pages/Login.js
@@ -1,34 +1,104 @@
-import { Button, TextField } from '@mui/material'
-import React, { Component } from 'react'
-import { signIn } from '../services/authService'
+import { Button, IconButton, InputAdornment, TextField } from '@mui/material';
+import { withStyles } from '@mui/styles';
+import React, { Component } from 'react';
+import { signIn } from '../services/authService';
+import '../styles/login.css';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-export default class Login extends Component {
+const styles = {
+    inputRoot: {
+        marginBottom: 15,
+    },
+};
+
+class Login extends Component {
 
     constructor(props) {
         super(props)
 
         this.state = {
-
+            username: '',
+            password: '',
+            isPasswordVisible: false,
         }
     }
 
     userSignIn = () => {
-        signIn();
+        const { username, password } = this.state;
+
+        signIn(username, password);
+    }
+
+    togglePasswordVisibility = () => {
+        this.setState({ isPasswordVisible: !this.state.isPasswordVisible });
+    }
+
+    onChangeValue = (newValue, key) => {
+        this.setState({
+            [key]: newValue,
+        })
     }
 
     render() {
+
+        const {
+            isPasswordVisible,
+            username,
+            password,
+        } = this.state;
+
+        const {
+            classes,
+        } = this.props;
+
         return (
-            <>
-                <TextField />
-                <TextField />
-                <Button
-                    color='primary'
-                    variant='contained'
-                    onClick={this.userSignIn}
-                >
-                    Login
-                </Button>
-            </>
+            <div className='aq-login-page-container'>
+                <div className='aq-login-form-container'>
+                    <div className='aq-login-text-container'>
+                        <TextField
+                            label='User Name'
+                            InputProps={{ className: classes.inputRoot }}
+                            value={username}
+                            onChange={event => this.onChangeValue(event.target.value, 'username')}
+                        />
+                        <TextField
+                            label='Password'
+                            type={isPasswordVisible ? 'text' : 'password'}
+                            value={password}
+                            onChange={event => this.onChangeValue(event.target.value, 'password')}
+                            InputProps={{
+                                className: classes.inputRoot,
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={this.togglePasswordVisibility}
+                                            onMouseDown={this.togglePasswordVisibility}
+                                            edge="end"
+                                        >
+                                            {isPasswordVisible ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+
+                    <div className='aq-login-button-container'>
+                        <Button
+                            color='primary'
+                            variant='contained'
+                            onClick={this.userSignIn}
+                            fullWidth
+                        >
+                            Login
+                        </Button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
+export default withStyles(styles)(Login);
--- React Web App/smart-aquaponic-web/src/services/authService.js
+++ React Web App/smart-aquaponic-web/src/services/authService.js
@@ -1,8 +1,36 @@
+import axios from "axios";
 
 
-export const signIn = () => {
-    localStorage.setItem('accessToken', 'testing-token');
-    window.location.replace('/login');
+export const signIn = (userName, password) => {
+
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => {
+            console.log(response && response.data);
+
+            localStorage.setItem('accessToken', 'testing-token');
+            window.location.replace('/login');
+        })
+        .catch(error => {
+            console.error('Request Failed', error);
+        });
+
+    // if(!userName || !password) return;
+
+    // const requestBody = {
+    //     userName,
+    //     password
+    // }
+
+    // axios.post('https://jsonplaceholder.typicode.com/todos/1', requestBody)
+    //     .then(response => {
+    //         console.log(response && response.data);
+
+    //         localStorage.setItem('accessToken', 'testing-token');
+    //         window.location.replace('/login');
+    //     })
+    //     .catch(error => {
+    //         console.error('Request Failed', error);
+    //     });
 }
 
 export const signOut = () => {
